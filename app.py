b1aaef1fcde4cc096ccd3de49bd53d5f4265658c
AbhiPonnada <140246057+AbhiPonnada@users.noreply.github.com>
2024-07-19 12:31:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 # Load the Excel file
-xls = pd.ExcelFile(r"C:/Users/Guest_User/Downloads/varsha.xlsx")
+xls = pd.ExcelFile(r"/data/data.xlsx")
 
 try:
     palm.configure(api_key="AIzaSyB-N4ZN2Hd7g3ftjPT23lgLUdUxph1-gkk")  # Replace with your actual API key
